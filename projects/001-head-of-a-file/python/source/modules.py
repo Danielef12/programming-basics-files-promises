1b0abfbddfe436144aeaa6e0005470fb37a25f4c
Daniele Fiocca <danielef12@hotmail.it>
2025-07-17 20:28:17 +0200

    add modify to modules.py

--- projects/001-head-of-a-file/python/source/modules.py
+++ projects/001-head-of-a-file/python/source/modules.py
@@ -1,11 +1,13 @@
 import os
-def reading_file(filepath):
+from typing import List
+
+
+def reading_file(filepath) -> List[str]:
     try:
         with open(filepath, 'r', encoding='utf-8') as file:
             return file.readlines()
     except FileNotFoundError:
         print(f"\n[ERROR] File not found: {filepath}")
-        return
 
 
 def head_of_a_file(filepath, line_display=10, show_filename=False):
